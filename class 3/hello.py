d4afe8e638dc884fb546238ee47a615c2b231d7b
naman pandey <naman03mgs@gmail.com>
2026-09-05 10:18:22 +0530

    new commits

---
+++ class 3/hello.py
@@ -0,0 +1,8 @@
+x = 20
+y = 23
+if x > 12:
+    print("suscess")
+    if(y > 20):
+        print("unscessfull")
+else:
+    print
---
+++ class 4/class4.py
@@ -0,0 +1,6 @@
+n = "artificial intelligence"
+for i in n:
+    if(i == "a" or i =="e" or i =="i" or i == "o" or i == "u"):
+        print(i, "its a vovel")
+    
+    
