7a73a403d44fd7f984b2733170aefa4ff6e275f2
naman pandey <naman03mgs@gmail.com>
2026-09-08 11:51:49 +0530

    new code added

--- class5/main.py
+++ class5/main.py
@@ -1,8 +1,14 @@
-x = 5
-print("x is", x)
-y = 7
-print("y is", y)
-x = "hello"
-print("now x is", x)
-print(x + "10")
-print("This line never runs")
+# x = 5
+# print("x is", x)
+# y = 7
+# print("y is", y)
+# x = "hello"
+# print("now x is", x)
+# print(x + "10")
+# print("This line never runs")
+
+print("Starting up the program...")
+print("Doing some setup...")
+Print("Hello!")
+print("All done.")
+
