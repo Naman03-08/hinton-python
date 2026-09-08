a8b75072b4e155e9dc8eea249e6d0831dba99996
naman pandey <naman03mgs@gmail.com>
2026-09-08 11:53:37 +0530

    new question solved

--- class5/main.py
+++ class5/main.py
@@ -7,8 +7,11 @@
 # print(x + "10")
 # print("This line never runs")
 
-print("Starting up the program...")
-print("Doing some setup...")
-Print("Hello!")
-print("All done.")
+# print("Starting up the program...")
+# print("Doing some setup...")
+# Print("Hello!")
+# print("All done.")
 
+# print("Starting...")
+# print(score)
+# print("This never runs")
