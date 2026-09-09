2e9a819b4b6898a7dd0d2d578713a25e005d726d
naman pandey <naman03mgs@gmail.com>
2026-09-09 23:47:49 +0530

    a new code added

--- class5/main.py
+++ class5/main.py
@@ -1,43 +1,44 @@
-# x = 5
-# print("x is", x)
-# y = 7
-# print("y is", y)
-# x = "hello"
-# print("now x is", x)
-# print(x + "10")
-# print("This line never runs")
+x = 5
+print("x is", x)
+y = 7
+print("y is", y)
+x = "hello"
+print("now x is", x)
+print(x + "10")
+print("This line never runs")
 
-# print("Starting up the program...")
-# print("Doing some setup...")
-# Print("Hello!")
-# print("All done.")
+print("Starting up the program...")
+print("Doing some setup...")
+Print("Hello!")
+print("All done.")
 
-# print("Starting...")
-# print(score)
-# print("This never runs")
+print("Starting...")
+print(score)
+print("This never runs")
 
-# print("Starting...")
-# age: int
-# print(age)
+print("Starting...")
+age: int
+print(age)
 
-# print("Calculating the class average...")
-# total = 100
-# count = 0
-# print(total / count)
-# print("Done")
+print("Calculating the class average...")
+total = 100
+count = 0
+print(total / count)
+print("Done")
 
-# x = 35 
-# y = 19
-# remender = x - int(x/y) * y
-# print(remender)
+x = 35 
+y = 19
+remender = x - int(x/y) * y
+print(remender)
 
-# print(35 % 19)
-# print(17 % 5)
-# print(20 % 4)
-# print(5 % 8)
+print(35 % 19)
+print(17 % 5)
+print(20 % 4)
+print(5 % 8)
 
 print(7.5 / 2)
 print(7.5 // 2)
 print(7.5 % 2)
 
 print(-2 ** -2 ** -2)
+
