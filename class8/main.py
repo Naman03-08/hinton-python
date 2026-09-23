87f3170803d4ea1b24a2e73a0c9e9d8407750f64
naman pandey <naman03mgs@gmail.com>
2026-09-23 10:10:53 +0530

    nwe question added and commented last questions

--- class8/main.py
+++ class8/main.py
@@ -9,12 +9,14 @@
 # print(sys.float_info.max)
 
 
-print(f"{0.1:.20f}")
-print(f"{0.2:.20f}")
-print(f"{0.3:.20f}")
-print(f"{0.1 + 0.2:.20f}")
+# print(f"{0.1:.20f}")
+# print(f"{0.2:.20f}")
+# print(f"{0.3:.20f}")
+# print(f"{0.1 + 0.2:.20f}")
 
-print(0.1 + 0.2 == 0.3) # direct ==
-print(round(0.1 + 0.2, 1) == 0.3) # round first
-import math
-print(math.isclose(0.1 + 0.2, 0.3)) # tolerance-based
+# print(0.1 + 0.2 == 0.3) # direct ==
+# print(round(0.1 + 0.2, 1) == 0.3) # round first
+# import math
+# print(math.isclose(0.1 + 0.2, 0.3)) # tolerance-based
+
+print(f"{0.1 + 0.2:.1f}")
