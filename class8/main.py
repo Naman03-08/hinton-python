db436b284ac5f82d8aebb7667d831aa97bf44d20
naman pandey <naman03mgs@gmail.com>
2026-09-23 10:05:15 +0530

    some more question attempted and commented few above question

--- class8/main.py
+++ class8/main.py
@@ -1,9 +1,15 @@
-print(0.1 + 0.2)
-print(0.1 + 0.2 == 0.3)
+# print(0.1 + 0.2)
+# print(0.1 + 0.2 == 0.3)
 
-print(f"{0.1:.55f}")
+# print(f"{0.1:.55f}")
 
-import sys
+# import sys
 
-print(sys.float_info.dig)
-print(sys.float_info.max)
+# print(sys.float_info.dig)
+# print(sys.float_info.max)
+
+
+print(f"{0.1:.20f}")
+print(f"{0.2:.20f}")
+print(f"{0.3:.20f}")
+print(f"{0.1 + 0.2:.20f}")
