26d16fea0ac36e053137183823e246921503ab5d
naman pandey <naman03mgs@gmail.com>
2026-09-23 09:23:46 +0530

    NEW COMMIT ADDED

--- class7/main.py
+++ class7/main.py
@@ -1,5 +1,7 @@
-print(2+3)
-print("2" + "3")
-print(int("2")+ 3)
-
-print(oct(42))
+READ = 0b100 # 4
+WRITE = 0b010 # 2
+EXECUTE = 0b001 # 1
+perms = READ | WRITE # user can read and write, but not execute
+print(bin(perms))
+print(bool(perms & READ)) # can the user read?
+print(bool(perms & EXECUTE)) # can the user execute?
